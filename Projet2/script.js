4abc87e9398b1d7a9fe0e700700fc433e958797d
Soundripper <88128696+Soundripper@users.noreply.github.com>
2022-01-14 13:53:38 +0100

    Add files via upload

---
+++ Projet2/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('click', function () {
+
+	// Bail if it's not a .nav-link
+	if (!event.target.classList.contains('nav-link')) return;
+
+	// Add the active class
+	event.target.classList.add('active');
+
+	// Get all nav links
+	var links = document.querySelectorAll('.nav-link');
+
+	// Loop through each link
+	for (var i = 0; i < links.length; i++) {
+
+		// If the link is the one clicked, skip it
+		if (links[i] === event.target) continue;
+
+		// Remove the .active class
+		links[i].classList.remove('active');
+
+	}
+
+}, false);
